9475c550ecb7f11134f89c86ff668f813626b778
DonaldWolfson <donald.j.wolfson@gmail.com>
2023-04-24 10:31:02 -0700

    Initialized Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'djs-personal-website',
+    appId: '1:883262944217:web:8c1e509c77d07f2db19fc8',
+    storageBucket: 'djs-personal-website.appspot.com',
+    apiKey: 'AIzaSyBEiRLy1HcGWJi3okgnF86DQpSkC7pDkY0',
+    authDomain: 'djs-personal-w ebsite.firebaseapp.com',
+    messagingSenderId: '883262944217',
+    measurementId: 'G-QFBFJCJXPT',
+  },
+  useEmulators: false,
+  production: true,
+};
